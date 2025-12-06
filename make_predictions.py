40b14f27f2627ba66e2b15502d01c9b83f70e955
DavidCastro88 <dcastrot@unal.edu.co>
2025-12-06 11:21:56 -0500

    Ejecucion de modelos

---
+++ make_predictions.py
@@ -0,0 +1,21 @@
+
+
+
+
+import mlflow
+import pandas as pd
+
+FILE_PATH = "data/winequality-red.csv"
+
+df = pd.read_csv(FILE_PATH)
+y = df['quality']
+x = df.drop(columns = ['quality'])
+
+## Debe verificarse el run_id del modelo de se quiere cargar 
+## Se puede obtener el run_id desde la interfaz de MLflow
+
+logged_model = "runs:/7c7ba941c567403680a9ab21e51870e8/model"
+loaded_model = mlflow.pyfunc.load_model(logged_model)
+y = loaded_model.predict(x)
+
+print(y)
